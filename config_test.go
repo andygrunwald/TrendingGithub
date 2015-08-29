27cd54b2c5046df30a5376e07135d614f93081a9
Andy Grunwald <andygrunwald@gmail.com>
2015-08-29 16:15:30 +0200

    Added first unit tests for configuration

---
+++ config_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewConfiguration_WithWrongFilenames(t *testing.T) {
+	mockData := []struct {
+		Testcase string
+		FileName string
+	}{
+		{"Empty filename", ""},
+		{"Not existing file", "./not-existing-file"},
+		{"Existing file, but wrong format", "./tests/config.xml"},
+	}
+
+	for _, data := range mockData {
+		c, err := NewConfiguration(data.FileName)
+		if c != nil {
+			t.Errorf("Testcase %s: Configuration is not nil -> %+v", data.Testcase, c)
+		}
+		if err == nil {
+			t.Errorf("Testcase %s: Err is nil, but expected an error", data.Testcase)
+		}
+	}
+
+}
+
+func TestNewConfiguration_WithExistingFile(t *testing.T) {
+	configMock := &Configuration{
+		Twitter: TwitterConfiguration{
+			ConsumerKey:       "Consumer-Key-ABC",
+			ConsumerSecret:    "Consumer-Secret-DEF",
+			AccessToken:       "Access-Token-Foo",
+			AccessTokenSecret: "Access-Token-Bar",
+		},
+		Redis: RedisConfiguration{
+			URL:  "127.0.0.1:6379",
+			Auth: "My-Secret-Password",
+		},
+	}
+
+	c, err := NewConfiguration("./tests/config.json")
+	if err != nil {
+		t.Errorf("Err is not nil")
+	}
+
+	if reflect.DeepEqual(c, configMock) == false {
+		t.Errorf("Configuration file is not the same as mock: %+v && %+v", c, configMock)
+	}
+}
