29faf35c679eca29a5822403f9f9508e326acf2e
Andy Grunwald <andygrunwald@gmail.com>
2015-08-29 17:39:00 +0200

    Removed flag.go and implemented it in main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"time"
@@ -15,6 +16,14 @@ const (
 )
 
 func main() {
+	var (
+		flagConfigFile = flag.String("config", "", "Path to configuration file.")
+		flagVersion    = flag.Bool("version", false, "Outputs the version number and exits.")
+		flagDebug      = flag.Bool("debug", false, "Outputs the tweet instead of tweet it. Useful for development.")
+	)
+
+	flag.Parse()
+
 	// Output the version and exit
 	if *flagVersion {
 		fmt.Printf("@TrendingGithub v%d.%d.%d\n", majorVersion, minorVersion, patchVersion)
