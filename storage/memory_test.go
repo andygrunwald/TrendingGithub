94bf1a8a23589c019e549df49fa8f506d7b612e0
Andy Grunwald <andygrunwald@gmail.com>
2015-08-30 17:03:41 +0200

    Added unit tests for memory storage

---
+++ storage/memory_test.go
@@ -0,0 +1,55 @@
+package storage
+
+import (
+	"testing"
+)
+
+var (
+	testProjectName = "andygrunwald/TrendingGithub"
+)
+
+func TestMarkRepositoryAsTweeted(t *testing.T) {
+	storage := MemoryStorage{}
+	pool := storage.NewPool("", "")
+	conn := pool.Get()
+
+	res, err := conn.MarkRepositoryAsTweeted(testProjectName, "1440946305")
+	if err != nil {
+		t.Fatalf("Error of marking repository: \"%s\"", err)
+	}
+
+	if res == false {
+		t.Fatal("Marking repositoriy failed, got false, expected true")
+	}
+}
+
+func TestIsRepositoryAlreadyTweeted(t *testing.T) {
+	storage := MemoryStorage{}
+	pool := storage.NewPool("", "")
+	conn := pool.Get()
+
+	res, err := conn.IsRepositoryAlreadyTweeted(testProjectName)
+	if err != nil {
+		t.Fatalf("First already tweeted check throws an error: \"%s\"", err)
+	}
+	if res == true {
+		t.Fatal("Repository was already tweeted, got true, expected false")
+	}
+
+	res, err = conn.MarkRepositoryAsTweeted(testProjectName, "1440946884")
+	if err != nil {
+		t.Fatalf("Error of marking repository: \"%s\"", err)
+	}
+
+	if res == false {
+		t.Fatal("Marking repositoriy failed, got false, expected true")
+	}
+
+	res, err = conn.IsRepositoryAlreadyTweeted(testProjectName)
+	if err != nil {
+		t.Fatalf("Second already tweeted check throws an error: \"%s\"", err)
+	}
+	if res == false {
+		t.Fatal("Repository was not already tweeted, got false, expected true")
+	}
+}
