ce0ad2ecda849654dd5ca2b4e278d07e7be88d61
Andy Grunwald <andygrunwald@gmail.com>
2015-08-30 17:44:05 +0200

    Replaces anaconda.API by TwitterAPI interface

--- twitter.go
+++ twitter.go
@@ -12,9 +12,14 @@ const (
 	DebugURLLength = 25
 )
 
+type TwitterAPI interface {
+	GetConfiguration(v url.Values) (conf anaconda.Configuration, err error)
+	PostTweet(status string, v url.Values) (tweet anaconda.Tweet, err error)
+}
+
 // Twitter is the datastructure to store the twitter client
 type Twitter struct {
-	API           *anaconda.TwitterApi
+	API           TwitterAPI
 	Configuration *anaconda.Configuration
 	Mutex         *sync.Mutex
 }
@@ -55,7 +60,7 @@ func (client *Twitter) SetupConfigurationRefresh(d time.Duration) {
 	}()
 }
 
-// tweet will .... tweet the text :D ... Badumts
+// Tweet will .... tweet the text :D ... Badumts
 func (client *Twitter) Tweet(text string) (*anaconda.Tweet, error) {
 	v := url.Values{}
 	tweet, err := client.API.PostTweet(text, v)
