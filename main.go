aa93240f9554b627e2bc87e3fb44be61cf93a442
Andy Grunwald <andygrunwald@gmail.com>
2016-12-19 20:10:31 +0100

    Moved StartTweeting to a different file

--- main.go
+++ main.go
@@ -55,6 +55,9 @@ func main() {
 	twitterClient.SetupConfigurationRefresh(configurationRefreshTime)
 
 	// Activate our growth hack feature
+	// From all of our followers, pick one and follow a friend of him.
+	// With this we hope the new person will get a message "TrendingGithub is following you"
+	// looks at our profile and follows us as well ;)
 	if *twitterFollowNewPerson {
 		twitterClient.SetupFollowNewPeopleScheduling(followNewPersonTime)
 	}
@@ -63,60 +66,4 @@ func main() {
 	defer storageBackend.Close()
 
 	StartTweeting(twitterClient, storageBackend)
-}
-
-// StartTweeting bundles the main logic of this bot.
-// It schedules the times when we are looking for a new project to tweet.
-// If we found a project, we will build the tweet and tweet it to our followers.
-// Because we love our followers ;)
-func StartTweeting(twitter *twitter.Twitter, storageBackend storage.Pool) {
-
-	// Setup tweet scheduling
-	ts := &TweetSearch{
-		Channel:   make(chan *Tweet),
-		Trending:  trendingwrap.NewTrendingClient(),
-		Storage:   storageBackend,
-		URLLength: twitter.Configuration.ShortUrlLengthHttps,
-	}
-	SetupRegularTweetSearchProcess(ts)
-
-	// Waiting for tweets ...
-	for tweet := range ts.Channel {
-		// Sometimes it happens that we won`t get a project.
-		// In this situation we try to avoid empty tweets like ...
-		//	* https://twitter.com/TrendingGithub/status/628714326564696064
-		//	* https://twitter.com/TrendingGithub/status/628530032361795584
-		//	* https://twitter.com/TrendingGithub/status/628348405790711808
-		// we will return here
-		// We do this check here and not in tweets.go, because otherwise
-		// a new tweet won`t be scheduled
-		if len(tweet.ProjectName) <= 0 {
-			log.Print("No project found. No tweet sent.")
-			continue
-		}
-
-		// In debug mode the twitter variable is not available, so we won`t tweet the tweet.
-		// We will just output them.
-		// This is a good development feature ;)
-		if twitter.API == nil {
-			log.Printf("Tweet: %s (length: %d)", tweet.Tweet, len(tweet.Tweet))
-
-		} else {
-			postedTweet, err := twitter.Tweet(tweet.Tweet)
-			if err != nil {
-				log.Println(err)
-			} else {
-				log.Printf("Tweet %s posted", postedTweet.IdStr)
-			}
-		}
-		ts.MarkTweetAsAlreadyTweeted(tweet.ProjectName)
-	}
-}
-
-func SetupRegularTweetSearchProcess(tweetSearch *TweetSearch) {
-	go func() {
-		for _ = range time.Tick(tweetTime) {
-			go tweetSearch.GenerateNewTweet()
-		}
-	}()
 }
--- tweets.go
+++ tweets.go
@@ -12,6 +12,7 @@ import (
 	trendingwrap "github.com/andygrunwald/TrendingGithub/trending"
 	"github.com/andygrunwald/go-trending"
 	"github.com/google/go-github/github"
+	"github.com/andygrunwald/TrendingGithub/twitter"
 )
 
 type TweetSearch struct {
@@ -236,6 +237,62 @@ func (ts *TweetSearch) MarkTweetAsAlreadyTweeted(projectName string) (bool, erro
 	}
 
 	return res, err
+}
+
+// StartTweeting bundles the main logic of this bot.
+// It schedules the times when we are looking for a new project to tweet.
+// If we found a project, we will build the tweet and tweet it to our followers.
+// Because we love our followers ;)
+func StartTweeting(twitter *twitter.Twitter, storageBackend storage.Pool) {
+
+	// Setup tweet scheduling
+	ts := &TweetSearch{
+		Channel:   make(chan *Tweet),
+		Trending:  trendingwrap.NewTrendingClient(),
+		Storage:   storageBackend,
+		URLLength: twitter.Configuration.ShortUrlLengthHttps,
+	}
+	SetupRegularTweetSearchProcess(ts)
+
+	// Waiting for tweets ...
+	for tweet := range ts.Channel {
+		// Sometimes it happens that we won`t get a project.
+		// In this situation we try to avoid empty tweets like ...
+		//	* https://twitter.com/TrendingGithub/status/628714326564696064
+		//	* https://twitter.com/TrendingGithub/status/628530032361795584
+		//	* https://twitter.com/TrendingGithub/status/628348405790711808
+		// we will return here
+		// We do this check here and not in tweets.go, because otherwise
+		// a new tweet won`t be scheduled
+		if len(tweet.ProjectName) <= 0 {
+			log.Print("No project found. No tweet sent.")
+			continue
+		}
+
+		// In debug mode the twitter variable is not available, so we won`t tweet the tweet.
+		// We will just output them.
+		// This is a good development feature ;)
+		if twitter.API == nil {
+			log.Printf("Tweet: %s (length: %d)", tweet.Tweet, len(tweet.Tweet))
+
+		} else {
+			postedTweet, err := twitter.Tweet(tweet.Tweet)
+			if err != nil {
+				log.Println(err)
+			} else {
+				log.Printf("Tweet %s posted", postedTweet.IdStr)
+			}
+		}
+		ts.MarkTweetAsAlreadyTweeted(tweet.ProjectName)
+	}
+}
+
+func SetupRegularTweetSearchProcess(tweetSearch *TweetSearch) {
+	go func() {
+		for _ = range time.Tick(tweetTime) {
+			go tweetSearch.GenerateNewTweet()
+		}
+	}()
 }
 
 // ShuffleStringSlice will randomize a string slice.
