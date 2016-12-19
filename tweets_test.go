0e8d6eba97135d637f659236cad7dc109d03e44c
Andy Grunwald <andygrunwald@gmail.com>
2016-12-19 19:47:03 +0100

    Moved ShuffleStringSlice and Crop into tweets.go

--- tweets_test.go
+++ tweets_test.go
@@ -111,3 +111,66 @@ func TestTweets_BuildTweet(t *testing.T) {
 		}
 	}
 }
+
+var testSlice = []string{"one", "two", "three", "four"}
+
+func TestUtility_ShuffleStringSlice_Length(t *testing.T) {
+	shuffledSlice := make([]string, len(testSlice))
+	copy(shuffledSlice, testSlice)
+	ShuffleStringSlice(shuffledSlice)
+
+	if len(testSlice) != len(shuffledSlice) {
+		t.Errorf("The length of slices are not equal. Got %d, expected %d", len(shuffledSlice), len(testSlice))
+	}
+}
+
+func TestUtility_ShuffleStringSlice_Items(t *testing.T) {
+	shuffledSlice := make([]string, len(testSlice))
+	copy(shuffledSlice, testSlice)
+	ShuffleStringSlice(shuffledSlice)
+
+	for _, item := range testSlice {
+		if IsStringInSlice(item, shuffledSlice) == false {
+			t.Errorf("Item \"%s\" not found in shuffledSlice: %+v", item, shuffledSlice)
+		}
+	}
+}
+
+func TestUtility_Crop(t *testing.T) {
+	testSentence := "This is a test sentence for the unit test."
+	textMock := []struct {
+		Content     string
+		Chars       int
+		AfterString string
+		Crop2Space  bool
+		Result      string
+	}{
+		{testSentence, 0, "", false, testSentence},
+		{testSentence, 99, "", false, testSentence},
+		{testSentence, 13, "", false, "This is a te"},
+		{testSentence, 13, "...", false, "This is a te..."},
+		{testSentence, 13, "", true, "This is a"},
+		{testSentence, 13, "...", true, "This is a..."},
+		{testSentence, -99, "", false, testSentence},
+		{testSentence, -13, "", false, "he unit test."},
+		{testSentence, -13, "...", false, "...he unit test."},
+		{testSentence, -13, "", true, "unit test."},
+		{testSentence, -13, "...", true, "...unit test."},
+	}
+
+	for _, mock := range textMock {
+		res := Crop(mock.Content, mock.Chars, mock.AfterString, mock.Crop2Space)
+		if res != mock.Result {
+			t.Errorf("Crop result is \"%s\", but expected \"%s\".", res, mock.Result)
+		}
+	}
+}
+
+func IsStringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
